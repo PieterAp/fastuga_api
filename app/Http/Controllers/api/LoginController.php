fb2f6a33edf562d6bdb883d8eb4b5b00f6e2395e
unknown <2191820@my.ipleiria.pt>
2022-11-17 19:11:46 +0000

    login feature refactor to use oauth/token request

--- app/Http/Controllers/api/LoginController.php
+++ app/Http/Controllers/api/LoginController.php
@@ -6,11 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 const PASSPORT_SERVER_URL = "http://localhost/api";
-const CLIENT_ID = 2;
-const CLIENT_SECRET = 'Mhqxi4mCP8KJhZOjqPhvgXxNAonjVeTFixyLbdDR';
 
 class LoginController extends Controller
 {
@@ -21,7 +20,24 @@ class LoginController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-  
+    public function __construct()
+    {
+        $this->client = DB::table('oauth_clients')->where('id', 2)->first();
+    }
+
+
+    private function passportAuthenticationData($email, $password)
+    {
+        return [
+            'grant_type' => 'password',
+            'client_id' => $this->client->id,
+            'client_secret' => $this->client->secret,
+            'username' => $email,
+            'password' => $password,
+            'scope' => '*'
+        ];
+    }
+
     public function login(Request $request)
     {
         //////////////////////////////////
@@ -61,43 +77,64 @@ class LoginController extends Controller
 
         // WRONG CREDENTIALS
         if (!$correctCredentials) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Login failed. Wrong credentials.',
-            ], 401);
+            return response('Login failed. Wrong credentials', 401);
         }
 
-        // LOGIN SUCESSFULL
-        //maybe fix this red but not sure how
-        //not sure if we need to verify is the user already have an token generated ou not
-        $token = auth()->user()->createToken('API Token')->accessToken;
-        return response(['user' => auth()->user(), 'token' => $token]);
+        //SUCESSEFULL LOGIN
+        $request = Request::create('/oauth/token', 'POST', $this->passportAuthenticationData(
+            $request->email,
+            $request->password
+        ));
+        $response = app()->handle($request);
+        $auth_server_response = json_decode((string) $response->content(), true);
+
+        return $auth_server_response;
     }
- 
+
     public function register(Request $request)
     {
-        $data = $request->validate([
-            'name' => 'required|max:255',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|confirmed',
-            'license_plate' => 'required',
-        ]);
 
+        $data = array(
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+            'password_confirmation' => $request->input('password_confirmation'),
+            'license_plate' => $request->input('license_plate'),
+        );
+
+        //email validation is acepting bruno@gmail.com2 should it?
         //validate license plate format
-        //confirmed - checks is password is equal to password_confirmation (needs this form input formats to work)
-        $data['password'] = bcrypt($request->password);
-        $user = User::create($data);
-        $token = $user->createToken('API Token')->accessToken;
+        $validator = Validator::make(
+            $data,
+            array(
+                'name' => 'required|max:255',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed',
+                'password_confirmation' => 'required|same:password',
+                'license_plate' => 'required'
+            )
+        );
 
-        return response([ 'user' => $user, 'token' => $token]);
+        if ($validator->fails()) {
+            $fieldsWithErrorMessagesArray = $validator->messages()->get('*');
+            return response()->json([
+                'status' => 'error',
+                'message' => $fieldsWithErrorMessagesArray,
+            ], 400);
+        }
+
+        $data['password'] = bcrypt($request->password);
+        User::create($data);
+
+        response()->json(['success' => 'success'], 200);
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         $accessToken = $request->user()->token();
         $token = $request->user()->tokens->find($accessToken);
         $token->revoke();
         $token->delete();
-        return response(['msg' => 'Token revoked'], 200);    
+        return response(['msg' => 'Token revoked'], 200);
     }
-
 }
